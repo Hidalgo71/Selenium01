d1cf3fd4ee8ecf3565e12130371357f8276aceb5
Hidalgo71 <y.ozdemir71@gmail.com>
2021-10-12 16:38:02 +0300

    Updated.

--- src/chechboxTesting.java
+++ src/chechboxTesting.java
@@ -1,3 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class chechboxTesting
 {
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.edge.driver", "D:\\Docs\\Drivers\\msedgedriver.exe");
+        WebDriver ieDriver = new EdgeDriver();
+        ieDriver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+
+
+    }
 }
