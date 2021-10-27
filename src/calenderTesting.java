f503dd9ea4c4c9931106639f963cbbbc423ed8c6
Hidalgo71 <y.ozdemir71@gmail.com>
2021-10-27 17:34:37 +0300

    Updated.

--- src/calenderTesting.java
+++ src/calenderTesting.java
@@ -14,12 +14,17 @@ public class calenderTesting
         chDriver.get("https://www.path2usa.com/travel-companions");
 
         chDriver.findElement(By.xpath("//input[@id='travel_date']")).click();                                           //Open calender. Wooow
+
+        while (!chDriver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("November"))      //Click "next" until found desired month. In this case it's "April"
+        {
+            chDriver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+        }
         List<WebElement> datesL = chDriver.findElements(By.className("day"));                                           //Getting all days as a list. It provides all dates to compare desired date leter. No hard-coding
         int countDays = chDriver.findElements(By.className("day")).size();
         for (int i = 0; i < countDays; i++)
         {
             String dateTxt = chDriver.findElements(By.className("day")).get(i).getText();                               //Getting date number as a text
-            if (dateTxt.equalsIgnoreCase("29"))
+            if (dateTxt.equalsIgnoreCase("29"))                                                              //Test Date
             {
                 chDriver.findElements(By.className("day")).get(i).click();
             }
