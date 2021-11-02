6ec0651bc889791dfa2fe6c160f8c14b95a3b69f
Hidalgo71 <y.ozdemir71@gmail.com>
2021-11-02 23:49:39 +0300

    Updated.

--- src/filtering.java
+++ src/filtering.java
@@ -1,7 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class filtering
 {
@@ -14,6 +20,9 @@ public class filtering
         WebDriver chDriver = new ChromeDriver();
         chDriver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 
-
+        chDriver.findElement(By.id("search-field")).sendKeys("Rice");                                           //Typing filter textbox
+        List<WebElement> vegNames = chDriver.findElements(By.xpath("//tr/td[1]"));                                           //Taking vegg names in 1st column as a list,
+        List<WebElement> filList = vegNames.stream().filter(v -> v.getText().contains("Rice")).collect(Collectors.toList()); //using filter select name 'rice' and store in a list
+        Assert.assertEquals(vegNames.size(), filList.size());
     }
 }
